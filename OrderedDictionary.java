2adb52fa0d083d404e60d96067d79667f008ed4c
Karnav Shah <shahkarnav37@gmail.com>
2020-04-19 21:15:14 -0400

    Delete OrderedDictionary.java

--- OrderedDictionary.java
+++
@@ -1,6 +0,0 @@
-public interface OrderedDictionary<E> extends Iterable<E> {
-    boolean insertElement(E data);
-    void removeElement(E data);
-    void findElement(E data);
-    // void closestKeyAfter();
-}
